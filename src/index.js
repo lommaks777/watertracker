39fd5e23049fc0bc5f9581b74cf12413e2dac3e3
Алексей Ломакин <aleksejlomakin@MacBook-Air-4.local>
2025-09-29 14:37:40 -0300

    Move WaterTracker.jsx into src and fix import for CRA constraints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import WaterTracker from '../WaterTracker';
+import WaterTracker from './WaterTracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
